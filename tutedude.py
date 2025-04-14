4b3aa005bf129f5727b1d19cd76277aef3f37fde
swamin07 <swaminkhond07@gmail.com>
2025-04-14 19:06:52 +0530

    Add files via upload

---
+++ tutedude.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter 1st number: "))
+num2=int(input("Enter 2nd number: "))
+
+print("Sum: ",num1 + num2)
+print("Difference: ",num1 - num2)
+print("product: ",num1 * num2)
+print("Divide: ",num1 / num2)
+
